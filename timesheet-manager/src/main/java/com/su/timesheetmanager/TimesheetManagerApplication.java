4073bdfbc3e493197333bfd77b8f47428fbe8554
vich1220 <viktor.chernikov@netcracker.com>
2022-02-06 17:04:29 +0400

    set up CORS policy

--- timesheet-manager/src/main/java/com/su/timesheetmanager/TimesheetManagerApplication.java
+++ timesheet-manager/src/main/java/com/su/timesheetmanager/TimesheetManagerApplication.java
@@ -2,6 +2,9 @@ package com.su.timesheetmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TimesheetManagerApplication {
@@ -10,4 +13,13 @@ public class TimesheetManagerApplication {
 		SpringApplication.run(TimesheetManagerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "DELETE", "PUT", "GET");
+			}
+		};
+	}
 }
